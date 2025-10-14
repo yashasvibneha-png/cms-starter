b1aee5bef63c6a6f414a6414a139c5ae70754d5b
yashasvibneha-png <yashasvibneha@gmail.com>
2025-10-15 02:45:52 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,17 +1,36 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
+import os
 
 bp = Blueprint('views', __name__)
+posts = []  # store posts temporarily in memory
+
+UPLOAD_FOLDER = 'static/uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @bp.route('/')
 def index():
-    return render_template('index.html')
-
+    return render_template('index.html', posts=posts)
 
 @bp.route('/create', methods=['GET', 'POST'])
 def create():
     if request.method == 'POST':
         title = request.form['title']
+        author = request.form['author']
         content = request.form['content']
-        # Save to DB if needed
-        return f"Post created: {title}"
+        image = request.files['image']
+
+        image_filename = None
+        if image and image.filename:
+            image_filename = os.path.join(UPLOAD_FOLDER, image.filename)
+            image.save(image_filename)
+
+        posts.append({
+            'title': title,
+            'author': author,
+            'content': content,
+            'image': image_filename
+        })
+
+        return redirect(url_for('views.index'))
+
     return render_template('create.html')
