ab4a8f8b79f7765a8ca8e51c781c6fc76253600d
yashasvibneha-png <yashasvibneha@gmail.com>
2025-10-15 00:38:43 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,1 +1,13 @@
-# (paste the views.py code block I shared previously here)
+from flask import Blueprint, render_template
+
+# Define the blueprint
+bp = Blueprint('views', __name__)
+
+@bp.route('/')
+def index():
+    return render_template('index.html')
+
+@bp.route('/create')
+def create():
+    return render_template('create.html')
+
