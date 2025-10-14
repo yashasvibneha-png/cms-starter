16cd399f0179b4801324b30d5a3c6e2948fb74d6
yashasvibneha-png <yashasvibneha@gmail.com>
2025-10-15 01:13:43 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,13 +1,26 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 
 # Define the blueprint
 bp = Blueprint('views', __name__)
 
+# In-memory store for posts (for testing)
+posts = []
+
 @bp.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', posts=posts)
 
-@bp.route('/create')
+@bp.route('/create', methods=['GET', 'POST'])
 def create():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        content = request.form.get('content')
+
+        if title and content:
+            posts.append({'title': title, 'content': content})
+            # Redirect to home page after creating post
+            return redirect(url_for('views.index'))
+
+    # For GET request, just show the form
     return render_template('create.html')
 
